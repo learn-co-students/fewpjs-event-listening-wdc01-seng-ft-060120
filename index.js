8501aea72ba2803296b12de703a7e3da6ac79c46
zato91 <zatokey@gmail.com>
2020-07-13 13:41:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let input = document.getElementById("input");
+
+
 function addingEventListener() {
+
+    input.addEventListener('click',function(event) {
+        alert("you cliked me");
+    });
 }
